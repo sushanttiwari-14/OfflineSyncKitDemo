a66f67ced69fc12b5886cb9c9b2ce51b6036c790
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-06-26 18:49:34 +0530

    Initial Commit

---
+++ OfflineSyncKitDemoTests/OfflineSyncKitDemoTests.swift
@@ -0,0 +1,19 @@
+//
+//  OfflineSyncKitDemoTests.swift
+//  OfflineSyncKitDemoTests
+//
+//  Created by sushant tiwari on 26/06/26.
+//
+
+import Testing
+@testable import OfflineSyncKitDemo
+
+struct OfflineSyncKitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
